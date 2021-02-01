e6526976bafdcb337ce1567893f2246ad1f84277
teghitsugaya <44857533+teghitsugaya@users.noreply.github.com>
2021-02-01 11:20:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1>Hallo DTC! <?php echo 'APPUiOV.2.0'?></h1>
+        <h1>Hallo DTC!!! <?php echo 'APPUiOV.5.0'?></h1>
         <h1>Hallo <?php echo gethostname();?></h1>
         <p class="lead">Datacomm APPUiO Example Source To Image PHP</p>
       </div>
