09b48686729c8408bb95ff117d3a2f2f5dc17dbd
teghitsugaya <44857533+teghitsugaya@users.noreply.github.com>
2021-02-01 09:33:35 +0700

    update index.php
    update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
 
-    <title>APPUiO PHP Demo</title>
+    <title>Welocome to APPUiO PHP Demo</title>
         
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -53,7 +53,7 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1>Hallo GUYS!<?php echo 'APPUiOV.2.0'?></h1>
+        <h1>Hallo Gais!<?php echo 'APPUiOV.2.0'?></h1>
         <h1>Hallo <?php echo gethostname();?></h1>
         <p class="lead">Datacomm APPUiO Example Source To Image PHP</p>
       </div>
